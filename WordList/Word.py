6b42ab7cc507d9852311569a149d6102d504553f
Runquan Ye <runquanye@macpro.local>
2020-09-05 15:51:14 -0400

    update

--- WordList/Word.py
+++ WordList/Word.py
@@ -10,42 +10,42 @@ Date: Sept/2020
 class Word():
 #constructor
     def __init__(self, name, catergory, meaning, root, derivativeWord, sourceWord, listNum, isHead):
-        if(bool(self.name and self.name.strip())):
+        if bool(self.name and self.name.strip()):
             self.name = name.lower()
         else:
             raise Exception.WordNameEmpty
 
-        if (bool(self.catergory and self.catergory.strip())):
+        if bool(self.catergory and self.catergory.strip()):
             self.catergory = catergory.lower()
         else:
             raise Exception.WordCategoryEmpty
 
-        if (bool(self.meaning and self.meaning.strip())):
+        if bool(self.meaning and self.meaning.strip()):
             self.meaning = meaning
         else:
             raise Exception.WordMeaningEmpty
 
-        if (bool(self.root and self.root.strip())):
+        if bool(self.root and self.root.strip()):
             self.root = root.lower()
         else:
             self.root = ''
 
-        if (bool(self.derivativeWord and self.derivativeWord.strip())):
+        if bool(self.derivativeWord and self.derivativeWord.strip()):
             self.derivativeWord = derivativeWord.lower()
         else:
             self.derivativeWord = ''
 
-        if (bool(self.sourceWord and self.sourceWord.strip())):
+        if bool(self.sourceWord and self.sourceWord.strip()):
             self.sourceWord = sourceWord.lower()
         else:
             self.sourceWord = ''
 
-        if (bool(self.listNum and self.listNum.strip())):
+        if bool(self.listNum and self.listNum.strip()):
             self.listNum = int(listNum)
         else:
             self.listNum = 0
 
-        if (bool(self.isHead and self.isHead.strip())):
+        if bool(self.isHead and self.isHead.strip()):
             self.isHead = json.loads(isHead.lower())
         else:
             self.isHead = False
@@ -53,71 +53,94 @@ class Word():
 
 #getters
     def getWord(self):
-        return self.name
+        return self.name.lower()
 
     def getCatergory(self):
-        return self.catergory
+        return self.catergory.lower()
 
     def getCatergoryList(self):
-        return list(filter(None, list(re.split('[,.; ]', self.catergory)))).sort()
+        #'n., Adj. ,vt., v' => ['adj', 'n', 'v', 'vt']
+        return sorted(list(filter(None, list(re.split('[,.; ]', self.catergory.lower())))))
 
     def getCatergoryString(self):
+        #'n., Adj. ,vt., v' => 'adj, n, v, vt'
         sortList = self.getCatergoryList()
-        return ', '.join([str(e) for e in sortList])
+        return ', '.join([str(e) for e in sortList]).lower()
 
     def getRoot(self):
-        return self.root
+        return self.root.lower()
 
     def getDerivativeWord(self):
-        return self.derivativeWord
+        return self.derivativeWord.lower()
 
     def getSourceWord(self):
-        return self.sourceWord
+        return self.sourceWord.lower()
 
     def getListNum(self):
         return self.listNum
 
     def getMeaning(self):
-        return self.meaning
+        return self.meaning.lower()
 
     def getMeaningList(self):
-        temp = list(filter(None, list(re.split('[; ]', self.meaning)))).sort()
-        return [list(filter(None, re.split('[., ]',i))) for i in temp]
+        #adj. 附属的, 辅助的; n. 子公司, 辅助者, 支流' => ['adj. 附属的, 辅助的', ' n. 子公司, 辅助者, 支流']
+        temp = re.split('[;]', self.meaning.lower())
+        #['adj. 附属的, 辅助的', ' n. 子公司, 辅助者, 支流'] => [['adj', '附属的', '辅助的'], ['n', '子公司', '辅助者', '支流']]
+        return sorted([list(filter(None, re.split('[., ]',i))) for i in temp])
 
     def getMeaningDist(self):
+        #temp => [['adj', '附属的', '辅助的'], ['n', '子公司', '辅助者', '支流']]
         temp = self.getMeaningList()
+        #temp => {'adj': ['附属的', '辅助的'], 'n': ['子公司', '辅助者', '支流']}
         return {i[0]:i[1:] for i in temp}
 
-    def getMeaningDistString(self):
+    def getMeaningDistCatString(self):
+        #temp => ['adj', '附属的', '辅助的'], ['n', '子公司', '辅助者', '支流']]
         temp = self.getMeaningList()
+        #temp => {'adj': '附属的, 辅助的', 'n': '子公司, 辅助者, 支流'}
         return {i[0]:', '.join(str(e) for e in i[1:]) for i in temp}
 
+    def getWordMeaningDist(self):
+        #{'Subsidiary': 'adj. 附属的, 辅助的; n. 子公司, 辅助者, 支流'}
+        return {self.name.lower() : self.meaning.lower()}
+
+    def getWordMeaningDistList(self):
+        #{'subsidiary': [['adj', '附属的', '辅助的'], ['n', '子公司', '辅助者', '支流']]}
+        return {self.name.lower() : self.getMeaningList()}
+
+    def getWordMeaningDistCatString(self):
+        #{'subsidiary': {'adj': '附属的, 辅助的', 'n': '子公司, 辅助者, 支流'}}
+        return {self.name.lower() : self.getMeaningDistCatString()}
 
 # setters
     def setCatergory(self, catergory):
-        if (bool(self.catergory and self.catergory.strip())):
+        if bool(self.catergory and self.catergory.strip()):
             self.catergory = catergory.lower()
 
+    def setCatergoryInList(self, catergoryList):
+        if catergoryList:
+            self.catergory = ', '.join(str(i).lower() for i in sorted(catergoryList))
+
     def setRoot(self, root):
-        if (bool(self.root and self.root.strip())):
+        if bool(self.root and self.root.strip()):
             self.root = root.lower()
 
     def setDerivativeWord(self, derivativeWord):
-        if (bool(self.derivativeWord and self.derivativeWord.strip())):
+        if bool(self.derivativeWord and self.derivativeWord.strip()):
             self.derivativeWord = derivativeWord.lower()
 
     def setSourceWord(self, sourceWord):
-        if (bool(self.sourceWord and self.sourceWord.strip())):
+        if bool(self.sourceWord and self.sourceWord.strip()):
             self.sourceWord = sourceWord.lower()
 
     def setListNum(self, listNum):
-        if (bool(self.listNum and self.listNum.strip())):
+        if bool(self.listNum and self.listNum.strip()):
             self.listNum = int(listNum)
 
     def setIsHead(self, isHead):
-        if (bool(self.isHead and self.isHead.strip())):
+        if bool(self.isHead and self.isHead.strip()):
             self.isHead = json.loads(isHead.lower())
 
     def setMeaning(self, meaning):
-        if (bool(self.meaning and self.meaning.strip())):
+        if bool(self.meaning and self.meaning.strip()):
             self.meaning = meaning
