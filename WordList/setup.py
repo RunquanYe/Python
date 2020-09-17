84df7ec4f8436b10a408c2a4374030af46ea93cb
Runquan Ye <runquanye@macpro.local>
2020-09-17 03:07:57 -0400

    update

---
+++ WordList/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WordList',
+    version='',
+    packages=[''],
+    url='https://github.com/RunquanYe/Python',
+    license='',
+    author='runquanye',
+    author_email='yer@mail.gvsu.edu',
+    description=''
+)
